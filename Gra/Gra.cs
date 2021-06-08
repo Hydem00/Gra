2b415ba02c3725bad4945759862847f103100cd1
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2021-06-08 15:47:27 +0200

    Podświetlanie randomowych buttonów

--- Gra/Gra.cs
+++ Gra/Gra.cs
@@ -14,6 +14,7 @@ namespace Gra
     {
         private Form1 _form1;
         int x, y;
+        int end = 3;
 
         Button[,] pola_button;
 
@@ -21,7 +22,7 @@ namespace Gra
         {
             InitializeComponent();
             _form1 = form1;
-            
+            buttonDalej.Hide();
         }
 
         void GenerujButtony(Panel panelGry, Button[,] buttony, int rozmiar)
@@ -58,13 +59,56 @@ namespace Gra
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonRozpocznij_Click(object sender, EventArgs e)
         {
             pola_button = new Button[_form1.wielkosc, _form1.wielkosc];
 
             GenerujButtony(panelGra, pola_button, _form1.wielkosc);
-
+            
             buttonRozpocznij.Hide();
+
+            buttonDalej.Show();
+        }
+
+        public void wait(int milliseconds)
+        {
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer");
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void buttonDalej_Click(object sender, EventArgs e)
+        {
+            Button podswietlony;
+            buttonDalej.Enabled = false;
+            Random rnd = new Random();
+            for (int i = 1; i <= end; i++)
+            {
+                podswietlony = pola_button[rnd.Next(_form1.wielkosc), rnd.Next(_form1.wielkosc)];
+                podswietlony.BackColor = Color.Yellow;
+                wait(1000);
+                podswietlony.BackColor = Color.White;
+            }
+            end++;
+            buttonDalej.Enabled = true;
+            
+            //int index = rnd.Next(_form1.wielkosc - 1, _form1.wielkosc -1);
+            
         }
     }
 }
