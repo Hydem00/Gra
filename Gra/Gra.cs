0a035574fc63e81acabd4f44d6825ab0fb772971
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2021-06-09 14:17:40 +0200

    Poprawione sprawdzanie wyboru

--- Gra/Gra.cs
+++ Gra/Gra.cs
@@ -16,6 +16,7 @@ namespace Gra
         int x, y;
         int end = 3;
         int i = 0;
+        int w = -1;
 
         Button[,] pola_button;
         List<Button> podswietlony = new List<Button>();
@@ -60,7 +61,11 @@ namespace Gra
             y = ((Button)sender).TabIndex % (int)_form1.wielkosc + 1;
             label1.Text = string.Format("x={0} y={1}", x, y);
 
+            w++;
+
             wybrany.Add(pola_button[x - 1, y - 1]);
+            
+            SprawdzWynik(w);
 
         }
 
@@ -114,10 +119,10 @@ namespace Gra
 
         private void buttonDalej_Click(object sender, EventArgs e)
         {
-            if (podswietlony.Any() && wybrany.Any())
-            {
-                SprawdzWynik();
-            }
+            //if (podswietlony.Any() && wybrany.Any())
+            //{
+            //    SprawdzWynik();
+            //}
 
             wybrany.Clear();
 
@@ -141,16 +146,19 @@ namespace Gra
 
         }
 
-        private void SprawdzWynik()
+        private void SprawdzWynik(int w)
         {
-            if (Enumerable.SequenceEqual(wybrany, podswietlony))
+            if (wybrany[w] == podswietlony[w])
             {
                 MessageBox.Show("Są równe", "Dobrze");
             }
             else
             {
                 MessageBox.Show("Nope", "Źle");
+                this.Close();
             }
+
+            
         }
     }
 }
